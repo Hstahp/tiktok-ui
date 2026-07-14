05e52d00675ffc464bb478b34713aa89eb6caa3f
Huy Phatt <phatle219@gmail.com>
2026-07-14 21:10:38 +0700

    10.18 Giải quyết vấn đề nhiều file index.js

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '~/components/Image';
+
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.username}`} className={cx('wrapper')}>
+            <Image
+                className={cx('avatar')}
+                src={
+                    data.avatar ||
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBJdd3XvR3LDCy5XyEb5iZJP5fLC3h6PTkzyVpRzQtwrYCIzr_a856iY&s=10'
+                }
+                alt={data.name}
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>{data.name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
+                </h4>
+                <span className={cx('username')}>{data.username}</span>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
+
+export default AccountItem;
