712d49e8a2f1f48eca35dff728052604ae4a5a93
Huy Phatt <phatle219@gmail.com>
2026-07-14 15:27:20 +0700

    10.17 Áp dụng PropTypes vào dự án

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -28,5 +29,9 @@ function AccountItem({ data }) {
         </Link>
     );
 }
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
 
 export default AccountItem;
